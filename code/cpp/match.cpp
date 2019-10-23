13a419b36d1cd40a690779e60611f3584d2eb2a1
Tong Shu Li <tongli@scripps.edu>
2019-10-22 17:05:24 -0700

    Propensity match in C++ with multiset
    Use a C++ multiset to perform 1:1 propensity score matching.
    
    Code takes around 3 seconds, which is roughly 2x as fast as the
    Python version.

--- code/cpp/match.cpp
+++ code/cpp/match.cpp
@@ -1,6 +1,9 @@
 // Last updated: 2019-10-22
 #include <iostream>
 #include <fstream>
+#include <set> // multiset
+#include <stdlib.h> // absolute value function
+#include <utility> // make_pair
 #include <vector>
 using namespace std;
 
@@ -37,10 +40,105 @@ pair<vector<PII>, vector<PII> > read_data()
 
 void match_pairs()
 {
+    float caliper = 0.05;
+
+    const int PRECISION = 1000000;
+    const int MAX_DIST = caliper * PRECISION;
+
     pair<vector<PII>, vector<PII> > data = read_data();
+
+    // 0.662 s to run to this line
 
     vector<PII> pos_people = data.first;
     vector<PII> neg_people = data.second;
+
+
+    puts("Building search tree...");
+
+    // this part is slow, takes 1.3 s
+    multiset<int> neg_scores;
+    for (vector<PII>::iterator it = neg_people.begin(); it != neg_people.end(); ++it)
+        neg_scores.insert(it->second);
+
+    // 1.936 s to run to this line
+
+
+    puts("Matching...");
+
+    vector<PII> matched_scores;
+
+    for (vector<PII>::iterator it = pos_people.begin(); (it != pos_people.end()) && (!neg_scores.empty()); ++it)
+    {
+        int target = it->second;
+
+
+
+        bool skip = false;
+        multiset<int>::iterator closest;
+
+
+        multiset<int>::iterator idx = neg_scores.lower_bound(target);
+
+        if (idx == neg_scores.end())
+        {
+            skip = true;
+            closest = prev(idx, 1);
+        }
+        else
+            if (idx == neg_scores.begin())
+            {
+                skip = true;
+                closest = idx;
+            }
+
+        if (!skip)
+        {
+            int cur_val = *idx;
+            int prev_val = *prev(idx, 1);
+
+            if ((target - prev_val) < (cur_val - target))
+                closest = prev(idx, 1);
+            else
+                closest = idx;
+        }
+
+        if (abs(target - (*closest)) > MAX_DIST)
+            continue;
+
+        neg_scores.erase(closest);
+
+        matched_scores.push_back(make_pair(it->first, *closest));
+    }
+
+    // 2.333 s to run to this line
+
+
+    // i don't know how to do the defaultdict
+    // for now i will just output the scores for reference
+
+    puts("Writing output...");
+
+    ofstream file_out("../../pipeline/cpp_orig_output.txt");
+
+    for (vector<PII>::iterator it = matched_scores.begin(); it != matched_scores.end(); ++it)
+        file_out << it->first << "\t" << it->second << endl;
+
+    file_out.close();
+
+    // 2.710 s to run to this line
+
+/*
+runtime:
+
+0.662 s to read data
+1.274 s to build search tree
+0.397 s to match
+0.377 s to save data
+
+total 2.710 s
+
+*/
+
 }
 
 int main()
